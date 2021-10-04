92ead568d8e864d5a9912d7197b312fa92d28f47
Bharti kumari <bhartikumari20@navgurukul.org>
2021-10-04 18:50:04 +0530

    created todos

--- controller/todos.js
+++ controller/todos.js
@@ -12,7 +12,43 @@ let createTodos = (req, res) => {
         res.send(err)
     });
 };
+//get my Todos 
+let getMyTodos = (req, res) => {
+    knex('users').select('users.id', 'users.name', 'users.email', 'users.age', 'cities.city_id', 'cities.cityName', 'todos.text', 'todos.dueDate' )
+    .join('cities', 'users.cityId', '=', 'cities.city_id')
+    .join('todos', 'users.id', '=', 'todos.assignedTo')
+    .then((allTodos) => {
+        let userArray = []
+        let userObj = {}
+         let i = 0
+         while(i<allTodos.length) {
+             var userData = {
+                 text: allTodos[i].text,
+                 assignedTo: {
+                    id: allTodos[i].id,
+                    name: allTodos[i].name,
+                    email: allTodos[i].email,
+                    age: allTodos[i].age,
+                    city: {
+                        cityName: allTodos[i].cityName,
+                        city_id: allTodos[i].city_id
+                    }
+                 },
+                 dueDate: allTodos[i].dueDate 
+             }
+             userArray.push(userData)
+             i++
+        }
+         userObj['todos'] = userArray
+         res.send(userObj)
+    }).catch((err) => {
+        res.send(err)
+    });
+};
+
+
 
 module.exports = {
-    createTodos
+    createTodos,
+    getMyTodos
 }
--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const Router = express.Router()
 const createTodosCon = require('../controller/todos')
+const auth = require('../middleware/auth')
 
 Router.post('/createTodos', createTodosCon.createTodos)
-
+Router.get('/getMyTodos',auth, createTodosCon.getMyTodos)
 
 module.exports = Router
+
